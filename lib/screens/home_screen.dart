f9ed436e3736b9fa4dadc5a34a632c9dc1dececc
JulioLeija30072000 <julio.metalica98@gmail.com>
2022-02-23 01:15:22 -0600

    [add] cap 108 terminado

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas_app/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Peliculas en cines'),
+        centerTitle: true,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search_outlined),
+            onPressed: () {},
+            ),
+        ],
+      ),
+      body: Column(
+        children: const [
+          //CardSwiper
+          CardSwiper()
+          //Listado horizontal de películas
+        ]
+      ),
+    );
+  }
+}
