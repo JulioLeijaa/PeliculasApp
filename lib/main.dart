33d855ed398b87977c5dd694f19d2b2d2227b8b7
JulioLeija30072000 <julio.metalica98@gmail.com>
2022-03-25 11:00:20 -0600

    [add] peticion http mapeada

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas_app/screens/screens.dart';
+import 'package:provider/provider.dart';
+import 'providers/movies_provider.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: ( _ ) => MoviesProvider(), lazy: false)
+      ],
+      child: const MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
---
+++ lib/providers/movies_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:peliculas_app/models/models.dart';
+
+class MoviesProvider extends ChangeNotifier {
+  final String _apiKey = '176c9f0185742349d0a7c4231c1668db';
+  final String _baseUrl = 'api.themoviedb.org';
+  final String _language = 'es-ES';
+
+  MoviesProvider() {
+    print('MoviesProvider instanciado');
+    getOnDisplayMovies();
+  }
+
+  getOnDisplayMovies() async {
+    var url = Uri.https(_baseUrl, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+    final response = await http.get(url);
+    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+
+    print(nowPlayingResponse.results[1].title);
+  }
+}
