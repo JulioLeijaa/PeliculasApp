db1754dc577fd0914ecef640b9e617b5446fa3c0
Luis Carrillo <luis.carrilloo.perez@gmail.com>
2022-04-03 15:57:59 -0500

    [ADD] firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:peliculas_app/screens/screens.dart';
 import 'package:provider/provider.dart';
 import 'providers/movies_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(const AppState());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const AppState());
+}
 
 class AppState extends StatelessWidget {
   const AppState({Key? key}) : super(key: key);
